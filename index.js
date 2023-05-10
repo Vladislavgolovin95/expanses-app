8bb30fbe9c1238942e379f112b856be19d040cac
Vladislav Golovin <vladislav.golovin95@mail.ru>
2023-05-10 21:52:31 +0300

    completed the first version of the application

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const expenses = [];
+const LIMIT = 10000;
+const CURRENCY = 'руб.';
+const STATUS_IN_LIMIT = 'всё хорошо';
+const STATUS_OUT_OF_LIMIT = 'всё плохо';
+const STATUS_OUT_OF_LIMIT_CLASSNAME = 'status_red';
 
 const inputNode = document.getElementById('js-input-expense');
 const buttonNode = document.getElementById('js-button-expense');
 const historyNode = document.getElementById('js-history-expense');
 const totalNode = document.getElementById('js-total-expense');
+const limitNode = document.getElementById('js-limit-expense');
+const statusNode = document.getElementById('js-status-expense');
+
+const expenses = [];
 
 buttonNode.addEventListener('click', function() {
     // 1. Получаем значение из поля ввода
@@ -22,7 +30,7 @@ buttonNode.addEventListener('click', function() {
     let expensesListHTML = '';
 
     expenses.forEach(element => {
-        expensesListHTML += `<li>${element} руб.</li>`;
+        expensesListHTML += `<li>${element} ${CURRENCY}</li>`;
     });
 
     historyNode.innerHTML = `<ol>${expensesListHTML}</ol>`;
@@ -35,4 +43,12 @@ buttonNode.addEventListener('click', function() {
     });
 
     totalNode.innerText = total;
+
+    // 5. Сравнение с лимитом и вывод статуса
+    if (total <= LIMIT) {
+        statusNode.innerText = STATUS_IN_LIMIT;
+    } else {
+        statusNode.innerText = STATUS_OUT_OF_LIMIT;
+        statusNode.classList.add(STATUS_OUT_OF_LIMIT_CLASSNAME);
+    }
 });
