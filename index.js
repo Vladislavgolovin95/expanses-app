17427cad534918c0cc23e76b3868d436fe0374ef
Vladislav Golovin <vladislav.golovin95@mail.ru>
2023-05-15 21:11:35 +0300

    version 1.5

--- index.js
+++ index.js
@@ -1,54 +1,77 @@
-const LIMIT = 10000;
-const CURRENCY = 'руб.';
 const STATUS_IN_LIMIT = 'всё хорошо';
 const STATUS_OUT_OF_LIMIT = 'всё плохо';
-const STATUS_OUT_OF_LIMIT_CLASSNAME = 'status_red';
 
-const inputNode = document.getElementById('js-input-expense');
-const buttonNode = document.getElementById('js-button-expense');
-const historyNode = document.getElementById('js-history-expense');
-const totalNode = document.getElementById('js-total-expense');
-const limitNode = document.getElementById('js-limit-expense');
-const statusNode = document.getElementById('js-status-expense');
+const inputNode = document.getElementById('expenseInput');
+const addButtonNode = document.getElementById('addButton');
+const clearButtonNode = document.getElementById('clearButton');
+const limitNode = document.getElementById('limitValue');
+const totalValueNode = document.getElementById('totalValue');
+const statusNode = document.getElementById('statusText');
+const historyList = document.getElementById('historyList');
 
-const expenses = [];
+let expenses = [];
+const limit = parseInt(limitNode.innerText);
 
-buttonNode.addEventListener('click', function() {
-    // 1. Получаем значение из поля ввода
-    if (!inputNode.value) {
+const getTotal = () => {
+    let sum = 0;
+    expenses.forEach((expense) => {
+        sum += expense;
+    });
+
+    return sum;
+};
+
+const renderStatus = () => {
+    const total = getTotal(expenses);
+    totalValueNode.innerText = total;
+
+    if (total <= limit) {
+        statusNode.innerText = STATUS_IN_LIMIT;
+        statusNode.className = 'stats__statusText_positive';
+    } else {
+        statusNode.innerText = `${STATUS_OUT_OF_LIMIT} (${limit - total} руб.)`;
+        statusNode.className = 'stats__statusText_negative';
+    }
+};
+
+const renderHistory = () => {
+    historyList.innerHTML = '';
+    expenses.forEach ((expense) => {
+        const historyItem = document.createElement('li');
+        historyItem.className = 'rub';
+        historyItem.innerText = expense;
+
+        historyList.appendChild(historyItem);
+    });
+};
+
+const render = () => {
+    renderStatus();
+    renderHistory();
+};
+
+const getExpenseFromUser = () => parseInt(inputNode.value);
+
+const clearInput = () => {
+    inputNode.value = '';
+};
+
+function addButtonHandler() {
+    const expense = getExpenseFromUser();
+    if (!expense) {
         return;
     }
-    
-    const expense = parseInt(inputNode.value);
-    
-    inputNode.value = '';
 
-    // 2. Сохраняем трату в список 
     expenses.push(expense);
 
-    // 3. Выводим новый список трат
-    let expensesListHTML = '';
+    render();
+    clearInput();
+}
 
-    expenses.forEach(element => {
-        expensesListHTML += `<li>${element} ${CURRENCY}</li>`;
-    });
+const clearButtonNodeHandler = () => {
+    expenses = [];
+    render();
+}
 
-    historyNode.innerHTML = `<ol>${expensesListHTML}</ol>`;
-
-    // 4. Посчитать сумму и вывести её
-    let total = 0;
-
-    expenses.forEach(element => {
-        total += (element);
-    });
-
-    totalNode.innerText = total;
-
-    // 5. Сравнение с лимитом и вывод статуса
-    if (total <= LIMIT) {
-        statusNode.innerText = STATUS_IN_LIMIT;
-    } else {
-        statusNode.innerText = STATUS_OUT_OF_LIMIT;
-        statusNode.classList.add(STATUS_OUT_OF_LIMIT_CLASSNAME);
-    }
-});
+addButtonNode.addEventListener('click', addButtonHandler);
+clearButtonNode.addEventListener('click', clearButtonNodeHandler);
